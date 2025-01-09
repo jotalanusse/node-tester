962748495f5b189004fad098683a1eb6cae3a6af
Jota Lanusse <jotalanusse@gmail.com>
2025-01-08 23:57:46 -0300

    feat: updated node config

--- src/config/nodes.config.ts
+++ src/config/nodes.config.ts
@@ -4,22 +4,22 @@ export const nodeConfigs: NodeConfig[] = [
   {
     name: 'node-1',
     ip: '52.67.127.42',
-    port:26657,
+    port: 12345,
   },
   {
     name: 'node-2',
     ip: '18.230.122.234',
-    port: 26657,
+    port: 12345,
   },
   {
     name: 'node-3',
     ip: '18.230.82.255',
-    port: 26657,
+    port: 12345,
   },
   {
     name: 'node-4',
     ip: '54.233.47.16',
-    port: 26657,
+    port: 12345,
   },
   // {
   //   name: 'single-node-1',
